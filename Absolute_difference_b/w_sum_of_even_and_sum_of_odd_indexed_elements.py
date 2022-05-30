608538df9de6b3dc1b4fdbc54c24a33607d78117
gayatrimamidipalli2003 <97426256+gayatrimamidipalli2003@users.noreply.github.com>
2022-05-30 13:08:51 +0530

    n=int(input()) a=list(map(int,input().split())) se=0 so=0 for i in range(0,n):     if i%2==0:         se+=a[i]     else:         so+=a[i] if se>so:     print(se-so) else:     print(so-se)

---
+++ Absolute_difference_b/w_sum_of_even_and_sum_of_odd_indexed_elements.py
@@ -0,0 +1,13 @@
+n=int(input())
+a=list(map(int,input().split()))
+se=0
+so=0
+for i in range(0,n):
+    if i%2==0:
+        se+=a[i]
+    else:
+        so+=a[i]
+if se>so:
+    print(se-so)
+else:
+    print(so-se)
