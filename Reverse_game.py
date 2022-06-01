3698464bea96cb12d9e5b8ba1c809fb67176911d
gayatrimamidipalli2003 <97426256+gayatrimamidipalli2003@users.noreply.github.com>
2022-06-01 22:40:57 +0530

    def rev(n):     rev=0     while n:         rem=n%10         rev=rev*10+rem         n//=10     return rev n=int(input()) a=list(map(int,input().split())) for i in a:     x=rev(i)     print(x,end=' ')

---
+++ Reverse_game.py
@@ -0,0 +1,12 @@
+def rev(n):
+    rev=0
+    while n:
+        rem=n%10
+        rev=rev*10+rem
+        n//=10
+    return rev
+n=int(input())
+a=list(map(int,input().split()))
+for i in a:
+    x=rev(i)
+    print(x,end=' ')
