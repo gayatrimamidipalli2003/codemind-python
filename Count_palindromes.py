26cd9771ebd1eca069c27b5edab3ad777b580f4b
gayatrimamidipalli2003 <97426256+gayatrimamidipalli2003@users.noreply.github.com>
2022-06-30 00:10:51 +0530

    def pali(n):     rev=0     temp=n     while temp:         rem=temp%10         rev=rev*10+rem         temp//=10     if rev==n:         return 1 n=int(input()) a=list(map(int,input().split())) c=0 for i in a:     if pali(i):         c+=1 print(c)

--- Count_palindromes.py
+++ Count_palindromes.py
@@ -1,14 +1,12 @@
 def pali(n):
     rev=0
     temp=n
-    while n:
-        rem=n%10
+    while temp:
+        rem=temp%10
         rev=rev*10+rem
-        n//=10
-    if rev==temp:
-        return True
-    else:
-        return False
+        temp//=10
+    if rev==n:
+        return 1
 n=int(input())
 a=list(map(int,input().split()))
 c=0
@@ -16,4 +14,3 @@ for i in a:
     if pali(i):
         c+=1
 print(c)
-    
