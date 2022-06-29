0b9dce77275040401d8cbe601f3f1f8af9bb765e
gayatrimamidipalli2003 <97426256+gayatrimamidipalli2003@users.noreply.github.com>
2022-06-29 23:50:23 +0530

    n=int(input()) f=0 for i in range(1,n):     if n==(i*(i+1)):         f=1         break if f==1:     print('YES') else:     print('NO')

--- Pronic_Number_or_Heteromecic_Number.py
+++ Pronic_Number_or_Heteromecic_Number.py
@@ -1,8 +1,10 @@
 n=int(input())
-for i in range(0,n):
-    if i*(i+1)==n:
-        print('YES')
+f=0
+for i in range(1,n):
+    if n==(i*(i+1)):
+        f=1
         break
+if f==1:
+    print('YES')
 else:
     print('NO')
-        
