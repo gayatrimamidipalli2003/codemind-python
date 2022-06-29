9c23da99ba8530c1765044b730c06bdc9c9be27d
gayatrimamidipalli2003 <97426256+gayatrimamidipalli2003@users.noreply.github.com>
2022-06-29 22:20:17 +0530

    def self_div(n):     c=d=0     temp=n     while n:         rem=n%10         c+=1         if rem!=0 and temp%rem==0:             d+=1         n//=10     if c==d:         print(temp,end=' ') a=int(input()) b=int(input()) for i in range(a,b+1):     self_div(i)

--- Self_Dividing_Numbers.py
+++ Self_Dividing_Numbers.py
@@ -1,15 +1,15 @@
+def self_div(n):
+    c=d=0
+    temp=n
+    while n:
+        rem=n%10
+        c+=1
+        if rem!=0 and temp%rem==0:
+            d+=1
+        n//=10
+    if c==d:
+        print(temp,end=' ')
 a=int(input())
 b=int(input())
 for i in range(a,b+1):
-    temp=i
-    c=0
-    d=0
-    while temp%10!=0:
-        rem=temp%10
-        temp//=10
-        if i%rem==0:
-            c+=1
-        d+=1
-    if c==d and i%10!=0:
-        print(i,end=' ')
-   
+    self_div(i)
