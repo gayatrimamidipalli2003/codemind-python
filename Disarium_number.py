0e1fd84faff46e926a0ecb36b26b053c20412dd0
gayatrimamidipalli2003 <97426256+gayatrimamidipalli2003@users.noreply.github.com>
2022-06-29 23:21:47 +0530

    import math n=int(input()) s=0 temp=n le=int(math.log10(n)+1) while n:     rem=n%10     s+=rem**le     le-=1     n//=10 if s==temp:     print(True) else:     print(False)

--- Disarium_number.py
+++ Disarium_number.py
@@ -1,15 +1,14 @@
 import math
 n=int(input())
-temp=n
 s=0
-le=int(math.log10(n))+1
+temp=n
+le=int(math.log10(n)+1)
 while n:
     rem=n%10
-    s=s+rem**le
-    n//=10
+    s+=rem**le
     le-=1
+    n//=10
 if s==temp:
     print(True)
 else:
     print(False)
-    
