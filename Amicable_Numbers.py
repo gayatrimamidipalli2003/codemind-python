810dea4e21b47bc9d4d2812496b7caaa9fab417a
gayatrimamidipalli2003 <97426256+gayatrimamidipalli2003@users.noreply.github.com>
2022-06-29 23:08:50 +0530

    a=int(input()) b=int(input()) x=1 sum=0 while x<=(a//2):     if a%x==0:         sum+=x     x+=1 if sum==b:     sum=0     x=1     while x<=(b//2):         if b%x==0:             sum+=x         x+=1 if sum==a:     print('Amicable') else:     print('Not Amicable')

--- Amicable_Numbers.py
+++ Amicable_Numbers.py
@@ -6,15 +6,14 @@ while x<=(a//2):
     if a%x==0:
         sum+=x
     x+=1
-y=1
 if sum==b:
     sum=0
-    while y<=(b//2):
-        if b%y==0:
-            sum+=y
-        y+=1
-    if sum==a:
-        print('Amicable')
+    x=1
+    while x<=(b//2):
+        if b%x==0:
+            sum+=x
+        x+=1
+if sum==a:
+    print('Amicable')
 else:
     print('Not Amicable')
-    
