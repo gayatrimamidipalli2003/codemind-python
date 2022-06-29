626bc85769bedd6f80dff6cc1b8381544290e54d
gayatrimamidipalli2003 <97426256+gayatrimamidipalli2003@users.noreply.github.com>
2022-06-29 22:06:12 +0530

    n=int(input()) s=0 while n:     s+=n%10     n//=10     if n==0 and s>9:         n=s         s=0 print(s)

--- Add_Digits.py
+++ Add_Digits.py
@@ -1,12 +1,9 @@
 n=int(input())
 s=0
-while 1:
+while n:
+    s+=n%10
+    n//=10
     if n==0 and s>9:
         n=s
         s=0
-    elif n==0 and s<10:
-        print(s)
-        break
-    rem=n%10
-    s+=rem
-    n//=10
+print(s)
