ad3e2dd2c8079dfe647f8ab8a78a57d4b99c53c5
gayatrimamidipalli2003 <97426256+gayatrimamidipalli2003@users.noreply.github.com>
2022-07-01 23:44:53 +0530

    s=input() s=s.lower() s=s.split() v='aeiou' r=[] c1=0 for i in s:     c=0     for j in i:         if j in v:             c+=1     r.append(c) for i in r:     if i==min(r):         c1+=1 print(c1)

---
+++ minimum_vowel_count_words.py
@@ -0,0 +1,16 @@
+s=input()
+s=s.lower()
+s=s.split()
+v='aeiou'
+r=[]
+c1=0
+for i in s:
+    c=0
+    for j in i:
+        if j in v:
+            c+=1
+    r.append(c)
+for i in r:
+    if i==min(r):
+        c1+=1
+print(c1)
