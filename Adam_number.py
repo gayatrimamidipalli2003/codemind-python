1b2e9a98c8b88f0219c36628a9396f0c58ae526f
gayatrimamidipalli2003 <97426256+gayatrimamidipalli2003@users.noreply.github.com>
2022-07-01 21:02:57 +0530

    n=int(input()) rev=revv=0 sq=n**2 while n:     rem=n%10     rev=rev*10+rem     n//=10 sor=rev**2 while sor:     rem=sor%10     revv=revv*10+rem     sor//=10 if sq==revv:     print(True) else:     print(False)

--- Adam_number.py
+++ Adam_number.py
@@ -1,18 +1,16 @@
 n=int(input())
-sq_n=n**2
-rev_n=0
+rev=revv=0
+sq=n**2
 while n:
     rem=n%10
-    rev_n=rev_n*10+rem
+    rev=rev*10+rem
     n//=10
-sq_rev_n=rev_n**2
-adam=0
-while sq_rev_n:
-    rem=sq_rev_n%10
-    adam=adam*10+rem
-    sq_rev_n//=10
-if sq_n == adam:
+sor=rev**2
+while sor:
+    rem=sor%10
+    revv=revv*10+rem
+    sor//=10
+if sq==revv:
     print(True)
 else:
     print(False)
-    
