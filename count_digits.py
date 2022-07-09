91f1c95748a1d09bbdd4dbd2b156b21c8e28525f
gayatrimamidipalli2003 <97426256+gayatrimamidipalli2003@users.noreply.github.com>
2022-07-09 14:55:03 +0530

    n=int(input()) a=list(map(int,input().split())) for i in a:     if i>=0:         i=str(i)         le=len(i)         print(le,end=' ')     else:         i=str(i)         le=len(i)         print((le-1),end=' ')

---
+++ count_digits.py
@@ -0,0 +1,11 @@
+n=int(input())
+a=list(map(int,input().split()))
+for i in a:
+    if i>=0:
+        i=str(i)
+        le=len(i)
+        print(le,end=' ')
+    else:
+        i=str(i)
+        le=len(i)
+        print((le-1),end=' ')
