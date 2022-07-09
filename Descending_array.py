67fc99a74799ca62b2dd883af022dbf6182e5f34
gayatrimamidipalli2003 <97426256+gayatrimamidipalli2003@users.noreply.github.com>
2022-07-09 15:09:38 +0530

    n=int(input()) a=list(map(int,input().split())) c=0 for i in range(0,n-1):     if a[i]>a[i+1]:         c+=1 if c==(n-1):     print('yes') else:     print('no')

--- Descending_array.py
+++ Descending_array.py
@@ -1,12 +1,11 @@
 n=int(input())
 a=list(map(int,input().split()))
 c=0
-for i in range(0,n):
-    if i==n-1:
-        break
+for i in range(0,n-1):
     if a[i]>a[i+1]:
         c+=1
-if c==n-1:
+if c==(n-1):
     print('yes')
 else:
     print('no')
+    
