541572f80c9ed1deb6264376bf8eeff3af99f3f1
gayatrimamidipalli2003 <97426256+gayatrimamidipalli2003@users.noreply.github.com>
2022-07-10 16:18:05 +0530

    n=input() num=int(n) s=str(num**2) if s.endswith(n):     print('Automorphic Number') else:     print('Not an Automorphic Number')

--- Automorphic_Number.py
+++ Automorphic_Number.py
@@ -1,21 +1,7 @@
-n=int(input())
-sq=n**2
-temp=n
-d=0
-n1=n2=0
-while temp:
-    rem=temp%10
-    n1=n1*10+rem
-    temp//=10
-while n:
-    d+=1
-    n//=10
-while d:
-    rem=sq%10
-    n2=n2*10+rem
-    sq//=10
-    d-=1
-if n1==n2:
+n=input()
+num=int(n)
+s=str(num**2)
+if s.endswith(n):
     print('Automorphic Number')
 else:
     print('Not an Automorphic Number')
