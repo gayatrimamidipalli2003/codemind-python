1b84e0bde227b0e2f4dee9a73bdea8525a01311e
gayatrimamidipalli2003 <97426256+gayatrimamidipalli2003@users.noreply.github.com>
2022-07-10 15:35:13 +0530

    def pali(n):     b=n[::-1]     if n==b:         return 1     else:         return 0 n=input() n=n.lower() if pali(n):     print(True) else:     print(False)

--- Palindrome_or_not.py
+++ Palindrome_or_not.py
@@ -1,7 +1,12 @@
-s=input()
-s=s.lower()
-rev=s[::-1]
-if s==rev:
+def pali(n):
+    b=n[::-1]
+    if n==b:
+        return 1
+    else:
+        return 0
+n=input()
+n=n.lower()
+if pali(n):
     print(True)
 else:
     print(False)
