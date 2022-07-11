febdf723101d55bf32bf18e4523185bb60a68d63
gayatrimamidipalli2003 <97426256+gayatrimamidipalli2003@users.noreply.github.com>
2022-07-11 18:51:45 +0530

    s=input() sum=0 for i in s:     if i=='1':         sum+=1     elif i=='2':         sum+=2     elif i=='3':         sum+=3     elif i=='4':         sum+=4     elif i=='5':         sum+=5     elif i=='6':         sum+=6     elif i=='7':         sum+=7     elif i=='8':         sum+=8     elif i=='9':         sum+=9 print(sum)

---
+++ Numbers_in_String.py
@@ -0,0 +1,22 @@
+s=input()
+sum=0
+for i in s:
+    if i=='1':
+        sum+=1
+    elif i=='2':
+        sum+=2
+    elif i=='3':
+        sum+=3
+    elif i=='4':
+        sum+=4
+    elif i=='5':
+        sum+=5
+    elif i=='6':
+        sum+=6
+    elif i=='7':
+        sum+=7
+    elif i=='8':
+        sum+=8
+    elif i=='9':
+        sum+=9
+print(sum)
