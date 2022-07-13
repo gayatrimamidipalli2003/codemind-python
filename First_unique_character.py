83637945dc7a4fa65177d87d9a8aa62edb0c9dd6
gayatrimamidipalli2003 <97426256+gayatrimamidipalli2003@users.noreply.github.com>
2022-07-14 00:57:35 +0530

    a=input().lower() a=a.replace(' ','') s=[] c=0 for i in a:     if a.count(i)==1:         s.append(i)         c=1 if c==0:     print('-1') else:     for i in s:         print(i)         break

--- First_unique_character.py
+++ First_unique_character.py
@@ -1,7 +1,14 @@
-n=input()
-for i in n:
-    if n.count(i)==1:
+a=input().lower()
+a=a.replace(' ','')
+s=[]
+c=0
+for i in a:
+    if a.count(i)==1:
+        s.append(i)
+        c=1
+if c==0:
+    print('-1')
+else:
+    for i in s:
         print(i)
         break
-else:
-    print(-1)
